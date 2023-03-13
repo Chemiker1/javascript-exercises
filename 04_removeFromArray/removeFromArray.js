11fa0afc22504619c6fbb99cba7f3ed251fae799
Chemiker1 <66435379+Chemiker1@users.noreply.github.com>
2023-03-13 21:30:48 +0100

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    for (const elementToRemove of elementsToRemove) {
+        array = array.filter(element => element !== elementToRemove);
+    }
+    return array;
 };
+
+console.log(removeFromArray([1,2,3,4], 3,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
